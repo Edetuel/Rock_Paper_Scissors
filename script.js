50fa88fd11e1cefe511873bfccf965c6465b1ee6
Edetuel <123456789+Edetuel@users.noreply.github.com>
2025-09-07 00:05:02 +0800

    logic redefined

--- script.js
+++ script.js
@@ -7,38 +7,48 @@ function getComputerChoice() {
 
 //get human's choice
 function getHumanChoice() {
-    let choice = prompt("Chose between Rock, Paper or Scissors: ");
 
-    return choice.toUpperCase();
+    return btn1, btn2, btn3
 }
 let computerScore = 0;
 let humanScore = 0;
 
+const outputContainer = document.querySelector("#outputContainer");
 
 //logic to determine if human choice wins or computers choice
 let  playRound = function(humanChoice, computerChoice) {
 
      if (humanChoice === computerChoice) {
         //tie condition
-        console.log(`It's a ties. You both picked ${humanChoice}`);
+        outputContainer.textContent = `It's a ties. You both picked ${humanChoice}`;
      } else if ((humanChoice === "rock") && (computerChoice === "scissors")) {
-        console.log("You win! Rock beats Scissors.");
+        outputContainer.textContent = "You win! Rock beats Scissors.";
         humanScore++ ;
     } else if ((humanChoice === "paper") && (computerChoice === "rock" )) {
-        console.log("You win! Paper beats Rock.");
+        outputContainer.textContent = "You win! Paper beats Rock.";
         humanScore++;
      } else if ((humanChoice === "scissors" )&& (computerChoice === "paper")) { 
-        console.log("You win! Scissor beats Paper");
+        outputContainer.textContent = "You win! Scissor beats Paper";
         humanScore++;
      } else {
         //computer's win in this case
-        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+        outputContainer.textContent = `You Lose! ${computerChoice} beats ${humanChoice}`;
         computerScore++;
      }
 
-     console.log(`Player's choice: ${humanChoice}`);
-     console.log(`computer's choice: ${computerChoice}`);
+     outputContainer.textContent = `Player's choice: ${humanChoice}`;
+     outputContainer.textContent = `computer's choice: ${computerChoice}`;
 }
+playRound();
+
+
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+
+btn1.addEventListener("click", playRound);
+btn2.addEventListener("click", playRound);
+btn3.addEventListener("click", playRound);
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
@@ -48,31 +58,37 @@ const computerSelection = getComputerChoice();
 
 function scores() {
      if (humanScore > computerScore) {
-        console.log(`Your score: ${humanScore} | Computer score ${computerScore}`);
+        outputContainer.textContent = `Your score: ${humanScore} | Computer score ${computerScore}`;
     } else if (computerScore > humanScore) {
-        console.log(`Your score: ${humanScore} | Computer score ${computerScore}`);
+        outputContainer.textContent = `Your score: ${humanScore} | Computer score ${computerScore}`;
     } else {
-        console.log("It's a tie");
+        outputContainer.textContent = "It's a tie!";
     }
 } 
 
 
 
-function playGame() {
-    console.log(`Rock, Paper, Scissors.
 
-This game is played for over 5 Rounds. Ties do not count.
-    `);
+function playGame() {
+    text.textContent = 
+"This game is played for over 5 Rounds. Ties do not count."
+    ;
     let i = 1;
     let roundCount = 5;
 
     while (i <= roundCount) {
-        console.log("Round " + i);
+        console.log ("Round"  + i);
+        
 
-        playRound(humanSelection, computerSelection);
+        playRound(computerSelection, humanSelection);
         i++;
+    
     }
 }
-       
-playGame();
+   
+const text = document.querySelector("#text");
+const round = document.querySelector("#round");
+
+
+playGame(playRound);
 console.log(scores());
